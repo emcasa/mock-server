090d9fc2f401ec369484559b5ea929883020aae0
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-08-27 15:27:54 -0300

    Address consistency

--- src/graphql/mocks/Address.js
+++ src/graphql/mocks/Address.js
@@ -1,34 +1,48 @@
+import flow from "lodash.flow";
 import faker from "faker/locale/pt_BR";
+import slugify$ from "slugify";
 import withSeed from "../helpers/withSeed";
 
-const neighborhoods = [
-  "Botafogo",
-  "Catete",
-  "Copacabana",
-  "Cosme Velho",
-  "Flamengo",
-  "Gávea",
-  "Humaitá",
-  "Ipanema",
-  "Itanhangá",
-  "Jardim Botânico",
-  "Joá",
-  "Lagoa",
-  "Laranjeiras",
-  "Leblon",
-  "Leme",
-  "São Conrado",
-  "Urca"
-];
+const slugify = str => slugify$(str, { lower: true });
+
+const neighborhood = () =>
+  faker.random.arrayElement([
+    "Botafogo",
+    "Catete",
+    "Copacabana",
+    "Cosme Velho",
+    "Flamengo",
+    "Gávea",
+    "Humaitá",
+    "Ipanema",
+    "Itanhangá",
+    "Jardim Botânico",
+    "Joá",
+    "Lagoa",
+    "Laranjeiras",
+    "Leblon",
+    "Leme",
+    "São Conrado",
+    "Urca"
+  ]);
 
 export default withSeed(() => ({
-  city: faker.address.city,
-  state: faker.address.stateAbbr,
   lat: faker.address.latitude,
   lng: faker.address.longitude,
-  neighborhood: () => faker.random.arrayElement(neighborhoods),
-  neighborhoodSlug: () => faker.random.arrayElement(neighborhoods),
   postalCode: faker.address.zipCode,
+  streetNumber: () => faker.random.number(1000).toString(),
+  neighborhood: neighborhood,
+  neighborhoodSlug: flow(
+    neighborhood,
+    slugify
+  ),
   street: faker.address.streetName,
-  streetNumber: () => faker.random.number(1000).toString()
+  street: flow(
+    faker.address.streetName,
+    slugify
+  ),
+  city: "Rio de Janeiro",
+  citySlug: "rio-de-janeiro",
+  state: "RJ",
+  stateSlug: "rj"
 }));
