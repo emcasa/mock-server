2cfa855a436da7273a03f1c75dd478be88f18d11
Nathan Queija <nathanqcam@gmail.com>
2018-08-28 14:11:16 -0300

    Update user mock

--- src/graphql/mocks/User.js
+++ src/graphql/mocks/User.js
@@ -1,8 +1,8 @@
 import faker from 'faker'
 
 export default () => ({
-  name: () => faker.name.findName(),
-  id: () => faker.random.number({min: 1, max: 200}) * 1000,
-  email: () => faker.internet.email(),
-  phone: () => faker.phone.phoneNumber()
+  name: 'Joh Doe',
+  id: () => 300,
+  email: 'johndoe@doe.com',
+  phone: '99999999'
 })
