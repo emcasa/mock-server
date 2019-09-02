e3a7a8fee4159a217a72c228ecc44a662bc9ea0b
Lucas Tulio <lucasctulio@gmail.com>
2019-09-02 17:51:14 -0300

    Remove interestTypes

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,11 +1,5 @@
 import { getDistrict, DISTRICTS } from "../mocks/District";
 
 export default {
-  districts: () => DISTRICTS.map(name => getDistrict(() => name)),
-  interestTypes: () => [
-    { id: "1", name: "Me ligue dentro de 5 minutos" },
-    { id: "2", name: "Me ligue em um horário específico" },
-    { id: "3", name: "Agendamento por e-mail" },
-    { id: "4", name: "Agendamento por Whatsapp" }
-  ]
+  districts: () => DISTRICTS.map(name => getDistrict(() => name))
 };
